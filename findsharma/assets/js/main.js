eeede38e25560c35319076e1cb38ce45a9cef184
Harshit Sharma <harshu729@gmail.com>
2020-08-17 22:21:25 +0530

    ins

---
+++ findsharma/assets/js/main.js
@@ -0,0 +1,15 @@
+/* 
+
+Project Name: findsharma 
+Author: Harshit Sharma | www.twitter.com/horshit | www.findsharma.me  | www.findsharma.tech
+
+*/
+
+$(document).ready(function() {
+	
+	setTimeout(function(){
+		$('body').addClass('loaded');
+		$('h1').css('color','#222222');
+	}, 3000);
+	
+});
