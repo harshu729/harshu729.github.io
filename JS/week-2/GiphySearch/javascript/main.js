d1fcd52fd4cda0f04a27e8d9b55da66a5f2bf791
Harshit Sharma <harshu729@gmail.com>
2020-08-28 18:53:40 +0530

    JS

---
+++ JS/week-2/GiphySearch/javascript/main.js
@@ -0,0 +1,61 @@
+/*Grab the input value*/
+
+
+document.querySelector(".js-go").addEventListener('click', function(e){
+	var input = document.querySelector("input").value;
+	var word = input.split(" ",2);
+	alert(word);
+	pushToDOM(word);
+});
+
+document.querySelector(".js-userinput").addEventListener('keyup',function(e){
+	
+	var input = document.querySelector("input").value;
+	
+	//if the key is pressed...
+	if(e.which === 13) {
+		pushToDOM(input);
+	}
+
+})
+
+
+/*Do the data stuff with the API*/
+
+var url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
+
+// AJAX Request
+var GiphyAJAXCall = new XMLHttpRequest();
+GiphyAJAXCall.open( 'GET', url );
+GiphyAJAXCall.send();
+
+
+GiphyAJAXCall.addEventListener('load', function(e) {
+
+	var data = e.target.response;
+	pushToDOM(data);
+
+});
+
+
+
+/*Show me the GIFs*/
+
+
+function pushToDOM(input) {
+
+	var response = JSON.parse(input);
+
+	var imageUrls = response.data;
+	
+	imageUrls.forEach(function(image){
+		var src = image.images.fixed_height.url;
+
+		console.log(src);
+		var container = document.querySelector(".js-container");
+		container.innerHTML += "<img src=" + src + ">"; 
+
+	});
+
+
+}
